eaf0b6999408adb45da3bda2b12a97a017f67dfe
thaslimShaikh <786shaikthaslim@gmail.com>
2026-03-15 15:59:51 +0530

    Implemented dashboard UI, goal setup improvements, nickname flow fix and final styling

--- app.py
+++ app.py
@@ -11,5 +11,10 @@ def goalsetup():
     nickname = request.args.get("nickname")
     return render_template("goalsetup.html", nickname=nickname)
 
+@app.route("/dashboard")
+def dashboard():
+    nickname = request.args.get("nickname")
+    return render_template("dashboard.html", nickname=nickname)
+
 if __name__ == "__main__":
     app.run(debug=True)
